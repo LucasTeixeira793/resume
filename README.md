947be6e56eba6a97d4f459296324c5fdbff03bf3
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2025-04-25 01:21:09 -0300

    fix: update 2025-04

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 1. Update contents of `src` folder to fit your needs. This item is explained [below](#update-contents).
 1. Commit and push your changes.
 1. GitHub Actions will automatically build the latest version and deploy it to GitHub Pages.
-1. Open `http://your-username.github.io/your-cv-repo`.
+1. Open `https://lucasteixeira793.github.io/resume/`.
 
 ### Update contents
 
--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Lucas Menezes Teixeira',
-  title: 'Estudante de Ciência da Computação',
+  title: 'Bacharel em Ciência da Computação',
   facts: {
     'Residência': '<a href="https://www.google.com/maps/place/Ribeir%C3%A3o+Pires,+SP/@-23.6937547,-46.4785759,12z/data=!3m1!4b1!4m6!3m5!1s0x94ce6d00aafd1e81:0xc51dec6c655d6878!8m2!3d-23.7116426!4d-46.4123086!16zL20vMGc3amQ2"><i class="fa fa-home fact-icon"></i>Ribeirão Pires - SP</a>',
     'WhatsApp': `<a href="https://wa.me/5511943791429?text=Ol%C3%A1+Lucas%2C+vi+seu+curr%C3%ADculo+e+gostaria+de+saber+se+voc%C3%AA+est%C3%A1+aberto+a+novas+oportunidades+profissionais.+Se+sim%2C+poder%C3%ADamos+marcar+uma+conversa%3F"><i class="fa-brands fa-whatsapp  fact-icon"></i>+55 (11) 94379-1429</a>`,
@@ -19,14 +19,14 @@ module.exports = {
     ['DevOps', 100],
     ['CI/CD', 70],
     ['Python', 68],
-    ['IOT', 68],
+    ['Machine Learning', 68],
     ['Muito mais...', 100]
   ],
   positions: [
     {
       title: 'Banco Safra SA - Analista Jr.',
       period: 'Jun/23 - Atualmente',
-      skills: ['.NET', 'Inovação', 'PL/SQL', 'Comunicação', 'Angular', 'Jenkins', 'Git', 'CI/CD'],
+      skills: ['.NET', 'PL/SQL', 'RabbitMQ', 'MongoDB', 'Kafka', 'Cloud', 'CI/CD', 'Monitoria'],
       contents: 
       "Desenvolvedor Full Stack, desempenhando papel no desenvolvimento de novas funcionalidades e na resolução de problemas sistêmicos relacionados ao produto de Empréstimo Consignado. Minha experiência envolve a manipulação de APIs em .NET e a criação de procedures em PL/SQL, enquanto utilizo ferramentas como Jenkins ou Gitlab CI, dependendo das necessidades do projeto, para a implementação de integração contínua e entrega contínua. Além disso, meu foco constante na comunicação tem sido essencial para garantir a colaboração eficaz em equipe e a entrega de soluções de alta qualidade."
     },
@@ -40,9 +40,9 @@ module.exports = {
   ],
   experience: [
     {
-      header: 'TCC: jan/24 - Atualmente',
+      header: 'TCC: jan/24 - dez/24',
       link: 'https://github.com/tcc-previsao-energia',
-      contents: 'O projeto visa desenvolver um sistema de previsão de preços de empresas de energia elétrica na bolsa de valores brasileira (B³), utilizando técnicas de machine learning com ênfase na arquitetura LSTM. A ideia está em integrar informações climáticas, econômicas e financeiras para melhorar a precisão das previsões e fornecer insights.'
+      contents: 'Desenvolvimento de modelos preditivos para o mercado de ações brasileiro utilizando técnicas de Machine Learning e Deep Learning, como ARIMA, Random Forest e LSTM. O projeto envolveu coleta e tratamento de dados históricos com a biblioteca yfinance, aplicação de decomposição sazonal e análise de desempenho dos modelos em múltiplos ativos financeiros (VALE3, PETR4 e ITUB3).'
     },
     {
       header: 'Kallistra: jan/23 - dez/23',
@@ -76,7 +76,7 @@ module.exports = {
     {
       title: 'São Paulo Tech School - SPTech',
       skills: ['Java + SpringBoot','Auto-controle Socioemocional','Trabalho em equipe','REST','CI/CD','R','AWS/Azure','Analytics','Scrum','IOT'],
-      type: 'Bacharelado em Ciências da Computação',
+      type: 'Bacharelado em Ciências da Computação.',
       period: '2021 - 2024',
       contents:[
         "Uma formação abrangente em programação e desenvolvimento. Durante meu curso, adquiri habilidades desde as bases fundamentais, como HTML/CSS, até o domínio de tecnologias avançadas, incluindo Node.js, __Java__ e criação de __API REST__. Além disso, fui introduzido aos conceitos essenciais de provisionamento de aplicações nas principais plataformas em nuvem, como __AWS e Azure__, juntamente com a compreensão de práticas de __Multi-cloud__. Também fui exposto ao uso de __Python e R__ para análises de dados, ampliando minha expertise tecnológica.",
@@ -87,7 +87,7 @@ module.exports = {
     {
       title: 'ETEC Profª M.C.M.',
       skills: ['Java','PHP','Desenvolvimento Web','MySQL','Git'],
-      type: 'Ensino médio integrado com técnico em Informática para Internet',
+      type: 'Ensino médio integrado com técnico em Informática para Internet.',
       period: '2018 - 2020',
       contents: [
         "Durante o ensino médio, participei de uma formação integrada em Informática para Internet, adquirindo habilidades em __desenvolvimento web__, sistemas operacionais e edição de imagens. Dominando linguagens de programação, frameworks e criação de websites interativos"
@@ -96,7 +96,7 @@ module.exports = {
     {
       title: 'Wizard',
       skills: ['Inglês','Criatividade','Apresentações'],
-      type: 'Ensino de inglês como segundo idioma',
+      type: 'Ensino de inglês como segundo idioma.',
       period: '2015 - 2022',
       contents: [
         "Durante meu período na Wizard, aprimorei minhas habilidades no idioma de forma abrangente e dinâmica. A cada semestre, tive a oportunidade de dar __palestras em inglês__ sobre diversos temas, o que me permitiu fortalecer minha fluência, ganhar confiança na comunicação oral e expandir meu conhecimento em diferentes áreas."
